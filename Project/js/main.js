ca407af3c388a42c123fee83e2353dd20a290f12
Severenchuk <42549871+Severenchuk@users.noreply.github.com>
2022-08-01 12:09:08 +0300

    Update main.js

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,3 +1,19 @@
+/*
+let nam1 = Number(prompt("введіть число MIN"));
+let nam2 = Number(prompt("введіть число MAX"));
+function randomInteger(min, max) {
+  let rand = Math.floor(min + Math.random() * (max - min) + 1)
+  return (rand);
+}
+console.log( randomInteger(nam1, nam2) );
+*/
+/*
+let num1 = prompt("введіть число MIN");
+let num2 = prompt("введіть число MAX");
+let rund = Math.floor(num1 + Math.random() * (num2 - num1))
+console.log(rund)
+*/
+
 $(document).ready(function(){
  $('.header').height($(window).height());
 })
@@ -6,3 +22,4 @@ $("body,html").animate({
   scrollTop:$("#" + $(this).data('value')).offset().top
 },1000)
  })
+
